03eed4fb1ea0345a8a5e8597c42f3e9388993d22
Ethan Averbukh <averbukh.ethan@gmail.com>
2022-03-01 12:36:20 -0700

    Added express.json to accept req.body data on Posts

--- controllers/vinylController.js
+++ controllers/vinylController.js
@@ -1,11 +1,27 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
-const Vinyl = require('../models/vinylSchema.js');
+const Vinyl = require("../models/vinylSchema.js");
 
-router.get('/', async(req,res)=>{
-    const vinyls = await Vinyl.find({});
-})
+router.get("/", async (req, res) => {
+  const vinyls = await Vinyl.find({});
+  res.json({
+    status: 200,
+    data: vinyls,
+  });
+});
 
+//req.body is allowed because of the middleware functionality
+router.post("/", async (req, res) => {
+    const vinyl = req.body;
+    await Vinyl.create({
+        artists: vinyl.artists,
+        album: vinyl.album
+    });
+    res.json({
+        status:200,
+        data: vinyl
+    })
+});
 
 module.exports = router;
